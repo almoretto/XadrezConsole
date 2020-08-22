64f77fd5f2075fb2c135e5d2f8e68296176fed48
amoretto <amoretto@outlook.com.br>
2020-08-22 14:21:59 -0300

    Criação de tratamento de exceções.

--- XadrezConsole/Tabuleiro/Tabuleiro.cs
+++ XadrezConsole/Tabuleiro/Tabuleiro.cs
@@ -16,10 +16,34 @@
         {
             return Pecas[linha, coluna];
         }
+        public Peca PecaPosicao(Posicao pos)
+        {
+            return Pecas[pos.Linha, pos.Coluna];
+        }
         public void ColocarPeca(Peca p, Posicao pos)
         {
             Pecas[pos.Linha, pos.Coluna] = p;
             p.PosicaoDaPeca = pos;
+        }
+        public bool ExistePecaNaPosicao(Posicao pos)
+        {
+            ValidaPosicao(pos);
+            return PecaPosicao(pos) != null;
+        }
+        public bool PosicaoValida(Posicao pos)
+        {
+            if (pos.Linha < 0 || pos.Linha > Linhas || pos.Coluna < 0 || pos.Coluna > Colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void ValidaPosicao(Posicao pos)
+        {
+            if (!PosicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição Inválida!");
+            }
         }
     }
 }
---
+++ XadrezConsole/Tabuleiro/TabuleiroException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tabuleiro
+{
+    class TabuleiroException : Exception
+    {
+        public TabuleiroException(string exception) : base(exception) { }
+    }
+}
