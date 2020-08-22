0b026f4fea0a062983d2dbb80a4f53bbff4498f8
amoretto <amoretto@outlook.com.br>
2020-08-22 11:52:12 -0300

    Criando Classe Peças, Tabuleiro e Posição

---
+++ XadrezConsole/Tabuleiro/Peca.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace Tabuleiro
+{
+    class Peca
+    {
+        public Posicao PosicaoDaPeca { get; set; }
+        public Cor CorDaPeca { get; protected set; }
+        public int QteMovimentos { get; protected set; }
+        public Tabuleiro TabuleiroDaPeca { get; protected set; }
+
+        public Peca(Posicao posicaoDaPeca, Cor corDaPeca, Tabuleiro tabuleiroDaPeca)
+        {
+            PosicaoDaPeca = posicaoDaPeca;
+            CorDaPeca = corDaPeca;
+            TabuleiroDaPeca = tabuleiroDaPeca;
+            QteMovimentos = 0;
+        }
+    }
+}
